911ceea31eae2c16ef3001d683320b00118ad580
Shafayath Jamil Rafi <shafayathj@gmail.com>
2025-09-29 20:26:53 +0600

    React Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Header Section</h1>
+      </header>
+      <main>
+        <h1>Main Content Section</h1>
+      </main>
+      <footer>
+        <h1>Footer Section</h1>
+      </footer>
+    </>
+  );
+}
+
+export default App;
