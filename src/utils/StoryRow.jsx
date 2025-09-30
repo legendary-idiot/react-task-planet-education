190b1e0bc7ef79f5ac6334bb0d978d22c4e61ff7
Shafayath Jamil Rafi <shafayathj@gmail.com>
2025-09-30 22:26:49 +0600

    Deployment Error Fixes

--- src/utils/StoryRow.jsx
+++ src/utils/StoryRow.jsx
@@ -1,24 +1,28 @@
-import CtaButton from "./Ctabutton";
+import CtaButton from "./CtaButton";
 
 const StoryRow = ({ story }) => (
-	<div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 items-start">
-		<div>
-			<img src={story.image} alt="story" className="w-full h-auto min-h-[380px] max-h-[450px] object-cover border-2 border-white" />
-		</div>
-		<div className="text-white">
-			<h3 className="text-2xl lg:text-4xl xl:text-5xl font-extrabold leading-tight">
-				{story.title}
-			</h3>
-			<p className="mt-4 text-white text-base max-w-xl">{story.desc}</p>
-			<div className="mt-5 flex flex-col md:flex-row gap-4">
-				{story.buttons.map((b) => (
-					<CtaButton key={b.label} variant={b.variant}>
-						{b.label}
-					</CtaButton>
-				))}
-			</div>
-		</div>
-	</div>
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 items-start">
+    <div>
+      <img
+        src={story.image}
+        alt="story"
+        className="w-full h-auto min-h-[380px] max-h-[450px] object-cover border-2 border-white"
+      />
+    </div>
+    <div className="text-white">
+      <h3 className="text-2xl lg:text-4xl xl:text-5xl font-extrabold leading-tight">
+        {story.title}
+      </h3>
+      <p className="mt-4 text-white text-base max-w-xl">{story.desc}</p>
+      <div className="mt-5 flex flex-col md:flex-row gap-4">
+        {story.buttons.map((b) => (
+          <CtaButton key={b.label} variant={b.variant}>
+            {b.label}
+          </CtaButton>
+        ))}
+      </div>
+    </div>
+  </div>
 );
 
 export default StoryRow;
